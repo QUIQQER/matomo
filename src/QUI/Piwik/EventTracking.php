1cb7329be8e96153f6db36d7d14f2eae021d5c67
Henning Leutz <leutz@pcsg.de>
2020-08-20 08:03:59 +0200

    refactor: require php matomo class

---
+++ src/QUI/Piwik/EventTracking.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace QUI\Piwik;
+
+use QUI;
+
+class EventTracking
+{
+    public static function onQuiqqerFrontendUsersUserRegister(
+        \QUI\Users\User $User,
+        \QUI\FrontendUsers\RegistrarInterface $Registrar,
+        string $registrationStatus
+    ) {
+        
+    }
+
+    public static function onQuiqqerFrontendUsersUserActivate(
+        \QUI\Users\User $User,
+        \QUI\FrontendUsers\RegistrarInterface $Registrar
+    ) {
+
+    }
+}
