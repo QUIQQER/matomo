d84f16bb5f20f72224b41012c9bc7965b4a4b363
Henning Leutz <leutz@pcsg.de>
2023-12-11 09:35:22 +0100

    feat: window.dataLayer bridge over new dataLayerPush event

--- bin/dataLayer.js
+++ bin/dataLayer.js
@@ -1,23 +1,13 @@
-whenQuiLoaded().then(() => {
+window.whenQuiLoaded().then(() => {
     'use strict';
 
-    if (typeof Proxy === 'undefined') {
-        return;
-    }
-
     window.dataLayer = window.dataLayer || [];
-
     window._mtm = window._mtm || [];
     window._mtm = window._mtm.concat(window.dataLayer);
 
-    // observe data layer and make the same changes in _mtm
-    window.dataLayer = new Proxy(window.dataLayer, {
-        set(target, property, value, receiver)
-        {
-            window._mtm[property] = value;
-
-            // Set value in array
-            return Reflect.set(target, property, value, receiver);
-        }
+    require(['qui/QUI'], function(QUI) {
+        QUI.addEvent('dataLayerPush', function(value) {
+            window._mtm.push(value);
+        });
     });
 });
