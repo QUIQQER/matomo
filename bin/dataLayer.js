dc10e761e184cac19922d2c1390743bb7918eec8
Henning Leutz <leutz@pcsg.de>
2023-12-18 13:00:48 +0100

    feat: implement automatic tracking of datalayer into _paq

--- bin/dataLayer.js
+++ bin/dataLayer.js
@@ -5,9 +5,18 @@ window.whenQuiLoaded().then(() => {
     window._mtm = window._mtm || [];
     window._mtm = window._mtm.concat(window.dataLayer);
 
+    if (window.MATOMO_TRACK_TO_PAQ) {
+        window._paq = window._paq || [];
+        window._paq = window._paq.concat(window.dataLayer);
+    }
+
     require(['qui/QUI'], function(QUI) {
         QUI.addEvent('dataLayerPush', function(value) {
             window._mtm.push(value);
+
+            if (window.MATOMO_TRACK_TO_PAQ) {
+                window._paq.push(value);
+            }
         });
     });
 });
